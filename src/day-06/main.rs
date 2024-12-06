fc2d4b2ef347f4264b60954ffe41df7ef33c9cda
David Knaack <davidkna@users.noreply.github.com>
2024-12-06 09:22:31 +0100

    day 06

---
+++ src/day-06/main.rs
@@ -0,0 +1,203 @@
+#![feature(test)]
+extern crate test;
+
+use bstr::ByteSlice;
+use fnv::FnvHashSet;
+use itertools::Itertools;
+use rayon::prelude::*;
+
+const INPUT: &[u8] = include_bytes!("../../inputs/day-06.txt");
+
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
+enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
+impl Direction {
+    fn turn_right(&self) -> Self {
+        match self {
+            Self::North => Self::East,
+            Self::East => Self::South,
+            Self::South => Self::West,
+            Self::West => Self::North,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Tile {
+    Empty,
+    Scaffold,
+}
+
+fn parse_input(input: &[u8]) -> ((i32, i32, Direction), Vec<Vec<Tile>>) {
+    let mut robot = (0, 0, Direction::North);
+
+    let map = input
+        .lines()
+        .enumerate()
+        .map(|(y, line)| {
+            line.iter()
+                .enumerate()
+                .map(|(x, &c)| match c {
+                    b'#' => Tile::Scaffold,
+                    b'.' => Tile::Empty,
+                    b'^' => {
+                        robot = (x as i32, y as i32, Direction::North);
+                        Tile::Empty
+                    }
+                    _ => unreachable!(),
+                })
+                .collect_vec()
+        })
+        .collect_vec();
+
+    (robot, map)
+}
+
+fn part_1(mut robot: (i32, i32, Direction), map: &[Vec<Tile>]) -> u32 {
+    let mut visisted = FnvHashSet::default();
+    visisted.insert((robot.0, robot.1));
+
+    loop {
+        let (x, y, dir) = robot;
+
+        let (next_x, next_y) = match dir {
+            Direction::North => (x, y - 1),
+            Direction::East => (x + 1, y),
+            Direction::South => (x, y + 1),
+            Direction::West => (x - 1, y),
+        };
+
+        if next_x < 0 || next_y < 0 {
+            break;
+        }
+
+        match map
+            .get(next_y as usize)
+            .and_then(|row| row.get(next_x as usize))
+        {
+            Some(Tile::Empty) => {
+                visisted.insert((next_x, next_y));
+                robot = (next_x, next_y, dir);
+            }
+            Some(Tile::Scaffold) => {
+                robot = (x, y, dir.turn_right());
+            }
+            None => break,
+        }
+    }
+
+    visisted.len() as u32
+}
+
+fn robot_run(mut robot: (i32, i32, Direction), map: &[Vec<Tile>]) -> bool {
+    let mut visisted = FnvHashSet::default();
+    visisted.insert(robot);
+
+    loop {
+        let (x, y, dir) = robot;
+
+        let (next_x, next_y) = match dir {
+            Direction::North => (x, y - 1),
+            Direction::East => (x + 1, y),
+            Direction::South => (x, y + 1),
+            Direction::West => (x - 1, y),
+        };
+
+        if next_x < 0 || next_y < 0 {
+            return false;
+        }
+
+        match map
+            .get(next_y as usize)
+            .and_then(|row| row.get(next_x as usize))
+        {
+            Some(Tile::Empty) => {
+                robot = (next_x, next_y, dir);
+            }
+            Some(Tile::Scaffold) => {
+                robot = (x, y, dir.turn_right());
+            }
+            None => return false,
+        }
+
+        if !visisted.insert(robot) {
+            return true;
+        }
+    }
+}
+
+fn part_2(robot: (i32, i32, Direction), map: &[Vec<Tile>]) -> u32 {
+    let (robot_x, robot_y, _) = robot;
+
+    (0..map.len())
+        .into_par_iter()
+        .map(|y| {
+            (0..map[y].len())
+                .into_par_iter()
+                .filter(|&x| {
+                    (x, y) != (robot_x as usize, robot_y as usize) && map[y][x] == Tile::Empty
+                })
+                .filter(|&x| {
+                    let mut map_copy = map.to_vec();
+                    map_copy[y][x] = Tile::Scaffold;
+
+                    robot_run(robot, &map_copy)
+                })
+                .count() as u32
+        })
+        .sum()
+}
+
+fn main() {
+    let (robot, map) = parse_input(INPUT);
+    println!("Part 1: {}", part_1(robot, &map));
+    println!("Part 2: {}", part_2(robot, &map));
+}
+
+#[cfg(test)]
+mod tests {
+    use std::hint::black_box;
+
+    use super::*;
+
+    const EXAMPLE: &str = "....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...";
+
+    #[test]
+    fn test_part_1() {
+        let (robot, map) = parse_input(EXAMPLE.as_bytes());
+        assert_eq!(part_1(robot, &map), 41);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let (robot, map) = parse_input(EXAMPLE.as_bytes());
+
+        assert_eq!(part_2(robot, &map), 6);
+    }
+
+    #[bench]
+    fn bench_part_1(b: &mut test::Bencher) {
+        let (robot, map) = parse_input(INPUT);
+        b.iter(|| part_1(black_box(robot), black_box(&map)));
+    }
+    #[bench]
+    fn bench_part_2(b: &mut test::Bencher) {
+        let (robot, map) = parse_input(INPUT);
+
+        b.iter(|| part_2(black_box(robot), black_box(&map)));
+    }
+}
